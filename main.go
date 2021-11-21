87d5077ee509fa0f859baec7e2ea8e2cd9c9bcfe
JAbduvohidov <jaga.vohidov@gmail.com>
2021-11-21 12:18:28 +0500

    health check added

--- main.go
+++ main.go
@@ -1,9 +1,19 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"time"
+)
 
 func main() {
 	r := gin.Default()
+
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+			"time":    time.Now(),
+		})
+	})
 
 	r.POST("/login", func(c *gin.Context) {
 		c.JSON(200, gin.H{
